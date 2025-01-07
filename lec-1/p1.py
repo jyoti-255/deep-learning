ff3ebf5880ca9a797e29b9b619e27f567132fc69
Jyoti Jha <157029864+jyoti-255@users.noreply.github.com>
2025-01-07 22:59:15 +0530

    Add files via upload

---
+++ lec-1/p1.py
@@ -0,0 +1,38 @@
+Deep Learning Vs Machine Learning
+1.Data Dependency(data hungry,data increses,performance increases)
+2.Hardware Dependency(CPU,matrix multiplication,DL need GPU(powerful)
+3.Training Time(training time is high,ml me training time is low,prediction time fast)
+4.Feature Selection(representation learning --feature extract kar leta haim,ml me manual feature extract karna hota hai)
+5.Interpretability(dl less,ml high)
+
+For less data ML wins and if have more data DL wins
+
+
+Why DL is growing now a days
+1:Hardware
+2:Architecture
+3:Framework
+4:Datasets
+5:Community
+ASIC-->
+TPU-Tensor processing unit(google)
+Edge TPU-Drive,Nonch,Smart Grass
+NPU-->Neural Processing Unit
+Start-->CPU
+Large-->GPU,TPU
+Smart phone-->Mobile Cpu,cpu,DSP,NPU
+
+..
+
+Framework
+train-->difficult-->scratch(code karne me jyada time jayega)-->Tensorflow(google)
+Pytorch-->Facebook
+
+2011-->Deep Learning
+Dist Belief--->Google
+2015-->Tensorflow(difficult to use tha)
+powerfull
+
+Keras(intermediate library)
+Caffe2(pytorch model)
+Pytorch(research work me jyada use hota hai)
